53dbee959bab67e25b0a2c20318a437fa50dde0a
Eric Dahl <eric@skeyelab.com>
2017-11-30 15:42:11 -0600

    bette schedule

--- jobs/gdax.rb
+++ jobs/gdax.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-SCHEDULER.every '1s', allow_overlapping: false do
+SCHEDULER.every '.5s', allow_overlapping: false do
   uri = URI.parse('https://api.coinbase.com/v2/prices/BTC-USD/spot')
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -13,7 +13,8 @@ SCHEDULER.every '1s', allow_overlapping: false do
   btc_price = '%.2f' % btc_price.delete(',').to_f
   #puts btc_price
   send_event('btcprice', { value: btc_price.to_f} )
-
+end
+SCHEDULER.every '.5s', allow_overlapping: false do
   uri = URI.parse('https://api.coinbase.com/v2/prices/ETH-USD/spot')
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -24,7 +25,8 @@ SCHEDULER.every '1s', allow_overlapping: false do
   eth_price = '%.2f' % eth_price.delete(',').to_f
   #puts btc_price
   send_event('ethprice', { value: eth_price.to_f} )
-
+end
+SCHEDULER.every '.5s', allow_overlapping: false do
   uri = URI.parse('https://api.coinbase.com/v2/prices/LTC-USD/spot')
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -33,7 +35,7 @@ SCHEDULER.every '1s', allow_overlapping: false do
   json_response = JSON.parse(response.body)
   ltc_price = json_response['data']['amount']
   ltc_price = '%.2f' % ltc_price.delete(',').to_f
-  #puts btc_price
+  puts ltc_price
   send_event('ltcprice', { value: ltc_price.to_f} )
 
 end
